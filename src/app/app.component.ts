d6cc057cc3060ac286d34152e3c316f65a8914c5
Ahmed Samir <ahmedsamir@Ahmeds-MacBook-Pro.local>
2018-11-17 14:56:50 +0200

    version X0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular7CrudFirebase';
+
+  itemsRef: AngularFireList<any>;
+  items: Observable<any[]>;
+
+  newItem = '';
+  editMsg: boolean = false;
+  editId: number;
+
+  constructor(db: AngularFireDatabase) {
+    this.itemsRef = db.list('messages');
+    // Use snapshotChanges().map() to store the key
+    this.items = this.itemsRef.snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+      )
+    );
+  }
+
+  addItem(newName: string) {
+    this.itemsRef.push({ text: newName });
+    this.newItem = '';
+  }
+
+  updateItem(key: string, newText: string) {
+    this.itemsRef.update(key, { text: newText });
+    this.editMsg = false;
+  }
+
+  deleteItem(key: string) {
+    this.itemsRef.remove(key);
+  }
+
 }
